a18b20a2a626e05ff3e898d2d8d6c98a48ada19b
Madina Tolubaeva <121172709+MadinaTO@users.noreply.github.com>
2023-03-09 14:28:46 +0600

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -17,19 +17,19 @@ async def ban(message: types.Message):
         await message.answer('Пиши в группу')
 
 
-async def ban_2(message: types.Message):
-    if message.chat.type != 'private':
-        if message.from_user.id not in ADMINS:
-            await message.answer('You are not my boss')
-        elif not message.from_user.username:
-            await message.answer('Команда должна быть @username')
+async def ban_username(message: types.Message):
+    try:
+        if message.chat.type != 'private':
+            message_words = message.text.split()
+            username = message_words[1]
+            await bot.kick_chat_member(message.chat.id, user_id=users[f'{username}'])
+            await message.answer('Он вышел сам!')
         else:
-            await bot.kick_chat_member(message.chat.id, message.from_user.username)
-            await message.answer(f'Он вышел сам')
-    else:
-        await message.answer('Пиши в группу')
+            await message.answer('пиши в группе!')
+    except KeyError:
+        await  message.answer(f'{username} ничего не написал зачем его кикать?')
 
 
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(ban, commands=['ban'], commands_prefix='!/')
-    dp.register_message_handler(ban_2, commands=['banban'], commands_prefix='!/')
+    dp.register_message_handler(ban_username, commands=['banban'], commands_prefix='!/')
